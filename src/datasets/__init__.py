3d25a80d6a11d2d714da1c8587cbaf72581833be
Юрий Павар <yuripavar361@gmail.com>
2024-12-26 20:29:14 +0300

    add for kaggle

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,3 +1,4 @@
 from src.datasets.common_voice import CommonVoiceDataset
 from src.datasets.custom_dir_audio_dataset import CustomDirAudioDataset
 from src.datasets.librispeech_dataset import LibrispeechDataset
+from src.datasets.librispeech_kaggle import LibrispeechDatasetKaggle
