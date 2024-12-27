76a982a9800cbedd830fee3d1fb61c83fc85d8a4
Юрий Павар <yuripavar361@gmail.com>
2024-12-27 16:32:21 +0300

    update zero_infinity and add max_grad_norm

--- src/loss/ctc_loss.py
+++ src/loss/ctc_loss.py
@@ -4,6 +4,9 @@ from torch.nn import CTCLoss
 
 
 class CTCLossWrapper(CTCLoss):
+    def __init__(self, blank=0, reduction='mean', zero_infinity=False):
+        super().__init__(blank=blank, reduction=reduction, zero_infinity=zero_infinity)
+        
     def forward(
         self, log_probs, log_probs_length, text_encoded, text_encoded_length, **batch
     ) -> Tensor:
