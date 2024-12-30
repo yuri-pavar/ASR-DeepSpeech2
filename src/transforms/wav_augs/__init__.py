d0f792c26dfe451cd35333e0b50b3dee000b1c59
Юрий Павар <yuripavar361@gmail.com>
2024-12-31 02:00:41 +0300

    add kaggle augs3

--- src/transforms/wav_augs/__init__.py
+++ src/transforms/wav_augs/__init__.py
@@ -1,1 +1,4 @@
 from src.transforms.wav_augs.gain import Gain
+from src.transforms.wav_augs.colored_noise import ColoredNoise
+from src.transforms.wav_augs.pitch_shift import PitchShift
+from src.transforms.wav_augs.resample import Resample
