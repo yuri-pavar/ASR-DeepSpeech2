fb8ed36ef1e2f27e763f1302a217a98485c44fd9
Юрий Павар <yuripavar361@gmail.com>
2024-12-30 17:30:20 +0300

    add kaggle input ds6

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,6 +1,5 @@
 from src.datasets.common_voice import CommonVoiceDataset
 from src.datasets.custom_dir_audio_dataset import CustomDirAudioDataset
 from src.datasets.librispeech_dataset import LibrispeechDataset
-# from src.datasets.librispeech_kaggle import LibrispeechDatasetKaggle
-from src.datasets.librispeech_kaggle import LibrispeechDatasetKaggle, LibrispeechDatasetKaggleInp
-# from src.datasets.librispeech_kaggle_inp import LibrispeechDatasetKaggleInp
+from src.datasets.librispeech_kaggle import LibrispeechDatasetKaggle
+from src.datasets.librispeech_kaggle_inp import LibrispeechDatasetKaggleInp
