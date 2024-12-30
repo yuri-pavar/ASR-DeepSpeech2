77da7fe0d1a718838d24bc9a673824462ab647e2
Юрий Павар <yuripavar361@gmail.com>
2024-12-30 17:16:36 +0300

    add kaggle input ds5

--- src/datasets/librispeech_kaggle.py
+++ src/datasets/librispeech_kaggle.py
@@ -12,6 +12,8 @@ from src.datasets.base_dataset import BaseDataset
 # FOR KAGGLE
 ROOT_PATH = Path("/kaggle/working/ASR-DeepSpeech2")  
 
+ROOT_PATH_INP = Path("/kaggle/working/ASR-DeepSpeech2")  
+
 URL_LINKS = {
     "dev-clean": "https://www.openslr.org/resources/12/dev-clean.tar.gz",
     "dev-other": "https://www.openslr.org/resources/12/dev-other.tar.gz",
@@ -59,6 +61,86 @@ class LibrispeechDatasetKaggle(BaseDataset):
 
     def _get_or_load_index(self, part):
         index_path = self._data_dir / f"{part}_index.json"
+        if index_path.exists():
+            with index_path.open() as f:
+                index = json.load(f)
+        else:
+            index = self._create_index(part)
+            with index_path.open("w") as f:
+                json.dump(index, f, indent=2)
+        return index
+
+    def _create_index(self, part):
+        index = []
+        split_dir = self._data_dir / part
+        if not split_dir.exists():
+            self._load_part(part)
+
+        flac_dirs = set()
+        for dirpath, dirnames, filenames in os.walk(str(split_dir)):
+            if any([f.endswith(".flac") for f in filenames]):
+                flac_dirs.add(dirpath)
+        for flac_dir in tqdm(
+            list(flac_dirs), desc=f"Preparing librispeech folders: {part}"
+        ):
+            flac_dir = Path(flac_dir)
+            trans_path = list(flac_dir.glob("*.trans.txt"))[0]
+            with trans_path.open() as f:
+                for line in f:
+                    f_id = line.split()[0]
+                    f_text = " ".join(line.split()[1:]).strip()
+                    flac_path = flac_dir / f"{f_id}.flac"
+                    t_info = torchaudio.info(str(flac_path))
+                    length = t_info.num_frames / t_info.sample_rate
+                    index.append(
+                        {
+                            "path": str(flac_path.absolute().resolve()),
+                            "text": f_text.lower(),
+                            "audio_len": length,
+                        }
+                    )
+        return index
+    
+
+class LibrispeechDatasetKaggleInp(BaseDataset):
+    def __init__(self, part, data_dir=None, *args, **kwargs):
+        assert part in URL_LINKS or part == "train_all"
+
+        if data_dir is None:
+            data_dir = ROOT_PATH / part / "LibriSpeech"
+            data_dir_inp = ROOT_PATH_INP / part / "LibriSpeech"
+            # data_dir.mkdir(exist_ok=True, parents=True)
+        self._data_dir = data_dir
+        self._data_dir_inp = data_dir_inp
+        if part == "train_all":
+            index = sum(
+                [
+                    self._get_or_load_index(part)
+                    for part in URL_LINKS
+                    if "train" in part
+                ],
+                [],
+            )
+        else:
+            index = self._get_or_load_index(part)
+
+        super().__init__(index, *args, **kwargs)
+
+    def _load_part(self, part):
+        arch_path = self._data_dir / f"{part}.tar.gz"
+        print(f"Loading part {part}")
+        wget.download(URL_LINKS[part], str(arch_path))
+        # wget.download(URL_LINKS[part], str(arch_path), cert=certifi.where())
+        # wget.download(URL_LINKS[part], str(arch_path), no_check_certificate=True)
+        shutil.unpack_archive(arch_path, self._data_dir)
+        for fpath in (self._data_dir / "LibriSpeech").iterdir():
+            shutil.move(str(fpath), str(self._data_dir / fpath.name))
+        os.remove(str(arch_path))
+        shutil.rmtree(str(self._data_dir / "LibriSpeech"))
+
+    def _get_or_load_index(self, part):
+        # index_path = self._data_dir / f"{part}_index.json"
+        index_path = self._data_dir_inp / f"{part}_index.json"
         if index_path.exists():
             with index_path.open() as f:
                 index = json.load(f)
