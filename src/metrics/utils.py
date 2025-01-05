26f6d7331cb249e6012ed33030d3e999953938ad
Юрий Павар <yuripavar361@gmail.com>
2025-01-05 19:51:40 +0300

    update metrics utils

--- src/metrics/utils.py
+++ src/metrics/utils.py
@@ -7,13 +7,21 @@ import editdistance
 
 def calc_cer(target_text, predicted_text) -> float:
     # TODO
-    if len(target_text):
-        return 1
+    # if len(target_text):
+    #     return 1
+    if not target_text:
+        if predicted_text:
+            return 1
+        return 0
     return editdistance.eval(target_text, predicted_text) / len(target_text)
 
 
 def calc_wer(target_text, predicted_text) -> float:
     # TODO
-    if len(target_text):
-        return 1
+    # if len(target_text):
+    #     return 1
+    if not target_text:
+        if predicted_text:
+            return 1
+        return 0
     return editdistance.eval(target_text.split(), predicted_text.split()) / len(target_text.split())
